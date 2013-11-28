f78e4169969a8378c0b48af5646bb54a426b8850
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2013-11-28 12:04:30 +0100

    Partially fix graph alignement on left

--- boostrap-dygraph/static/js/tsung.graph.js
+++ boostrap-dygraph/static/js/tsung.graph.js
@@ -5,32 +5,32 @@
 
 
 gtrans = new Dygraph(document.getElementById("tr_mean"),
-		     "csv_data/graphes-Transactions-mean.csv",
-		     graphOptions("Transactions duration","tr_mean_val","msec", true)
-		    );
+		             "csv_data/graphes-Transactions-mean.csv",
+		             graphOptions("Transactions duration","tr_mean_val","msec", true)
+		            );
 
 gtransrate = new Dygraph(document.getElementById("transrate"),
-			 "csv_data/graphes-Transactions-rate.csv",
-			 graphOptions("Transactions rate","transrate_val","transactions/sec", false)
-			);
+			             "csv_data/graphes-Transactions-rate.csv",
+			             graphOptions("Transactions rate","transrate_val","transactions/sec", false)
+			            );
 
 gperf = new Dygraph(document.getElementById("perfs_mean"),
-		    "csv_data/graphes-Perfs-mean.csv",
-		    graphOptions("","tr_perfs_val","msec", false) 
-		   );
+		            "csv_data/graphes-Perfs-mean.csv",
+		            graphOptions("Requests response time","tr_perfs_val","msec", false) 
+		           );
 
 
 
 greqrate = new Dygraph(document.getElementById("reqrate"),
-		       "csv_data/graphes-Perfs-rate.csv",
-		       graphOptions("requests","reqrate_val","requ/sec", true)
-		      );
+		               "csv_data/graphes-Perfs-rate.csv",
+		               graphOptions("Requests rate","reqrate_val","requ/sec", true)
+		              );
 
 
 gusersim = new Dygraph(document.getElementById("usersim"),
-		       "csv_data/graphes-Users-simultaneous.csv",
-		       graphOptions("", "usersim_val","users")
-		      );
+		               "csv_data/graphes-Users-simultaneous.csv",
+		               graphOptions("", "usersim_val","users")
+		              );
 
 garrivalrate = new Dygraph(document.getElementById("arrivalrate"),
 			   "csv_data/graphes-Users_Arrival-rate.csv",
